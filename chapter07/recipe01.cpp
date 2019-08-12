25e4db9e8936e6343b68378e01d1cd09b39b3ce3
Rian Quinn <rianquinn@localhost.localdomain>
2019-08-12 13:28:25 -0600

    working on chapter 7

--- chapter07/recipe01.cpp
+++ chapter07/recipe01.cpp
@@ -22,7 +22,124 @@
 // -----------------------------------------------------------------------------
 #ifdef EXAMPLE01
 
-#include <iostream>
+#define CATCH_CONFIG_MAIN
 #include <catch.hpp>
+
+#include <iostream>
+
+TEST_CASE("the answer")
+{
+    std::cout << "The answer is: 42\n";
+}
+
+// test cases: 1 | 1 passed
+// assertions: - none -
+
+#endif
+
+// -----------------------------------------------------------------------------
+#ifdef EXAMPLE02
+
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+TEST_CASE("sort a vector")
+{
+    std::vector<int> v{4, 8, 15, 16, 23, 42};
+    REQUIRE(v.size() == 6);
+
+    SECTION("sort descending order") {
+        std::sort(v.begin(), v.end(), std::greater<int>());
+
+        CHECK(v.front() == 42);
+        CHECK(v.back() == 4);
+    }
+
+    SECTION("sort ascending order") {
+        std::sort(v.begin(), v.end(), std::less<int>());
+
+        CHECK(v.front() == 4);
+        CHECK(v.back() == 42);
+    }
+ }
+
+// ===============================================================================
+// All tests passed (6 assertions in 1 test case)
+
+#endif
+
+// -----------------------------------------------------------------------------
+#ifdef EXAMPLE03
+
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+void foo(int val)
+{
+    if (val != 42) {
+        throw std::runtime_error("The answer is: 42");
+    }
+}
+
+TEST_CASE("the answer")
+{
+    CHECK_NOTHROW(foo(42));
+    REQUIRE_NOTHROW(foo(42));
+
+    CHECK_THROWS(foo(0));
+    CHECK_THROWS_AS(foo(0), std::runtime_error);
+    CHECK_THROWS_WITH(foo(0), "The answer is: 42");
+
+    REQUIRE_THROWS(foo(0));
+    REQUIRE_THROWS_AS(foo(0), std::runtime_error);
+    REQUIRE_THROWS_WITH(foo(0), "The answer is: 42");
+}
+
+// ===============================================================================
+// All tests passed (8 assertions in 1 test case)
+
+#endif
+
+// -----------------------------------------------------------------------------
+#ifdef EXAMPLE04
+
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+void foo(int val)
+{
+    if (val != 42) {
+        throw std::runtime_error("The answer is: 42");
+    }
+}
+
+TEST_CASE("the answer")
+{
+    CHECK_NOTHROW(foo(42));
+    REQUIRE_NOTHROW(foo(42));
+
+    CHECK_THROWS(foo(0));
+    CHECK_THROWS_AS(foo(0), std::runtime_error);
+    CHECK_THROWS_WITH(foo(0), "The answer is: 42");
+
+    REQUIRE_THROWS(foo(0));
+    REQUIRE_THROWS_AS(foo(0), std::runtime_error);
+    REQUIRE_THROWS_WITH(foo(0), "The answer is: 42");
+}
+
+// ===============================================================================
+// All tests passed (8 assertions in 1 test case)
 
 #endif
